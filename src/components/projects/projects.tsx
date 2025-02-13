5be09850db8a18dad2325d6840b2ea9cb2ee122e
Farouk Adedamola <faroukadedamola1@gmail.com>
2025-02-13 14:18:49 +0100

    testing button

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -1,98 +1,98 @@
-// 'use client';
+'use client';
 
-// import Image from 'next/image';
-// import Link from 'next/link';
-// import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
-// import { motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 // import { Button } from '@/components/Button/button';
-// import Text from '@/components/Text/text';
-// import { ProjectProps } from '@/utils/data';
+import Text from '@/components/Text/text';
+import { ProjectProps } from '@/utils/data';
 
-// type ProjectSectionProps = {
-//   data: ProjectProps[];
-// };
+type ProjectSectionProps = {
+  data: ProjectProps[];
+};
 
-// const ProjectsSection = ({ data }: ProjectSectionProps) => {
-//   const newData = data.slice(0, 2);
-//   const router = useRouter();
+const ProjectsSection = ({ data }: ProjectSectionProps) => {
+  const newData = data.slice(0, 2);
+  const router = useRouter();
 
-//   return (
-//     <section className="section_layout">
-//       <Text font="figtree" weight="bold" size="xs" className="mb-8">
-//         Featured Projects{' '}
-//         <span className="inline-block h-4 w-4 rounded-full bg-emerald-light"></span>
-//       </Text>
-//       <div className="mb-12 grid grid-cols-1 gap-8">
-//         {newData.map((project, index) => (
-//           <ProjectCard key={project.id} project={project} index={index} />
-//         ))}
-//       </div>
-//       <div className="flex justify-center">
-//         <Button onClick={() => router.push('/projects')}>
-//           View More Projects
-//         </Button>
-//       </div>
-//     </section>
-//   );
-// };
+  return (
+    <section className="section_layout">
+      <Text font="figtree" weight="bold" size="xs" className="mb-8">
+        Featured Projects{' '}
+        <span className="inline-block h-4 w-4 rounded-full bg-emerald-light"></span>
+      </Text>
+      <div className="mb-12 grid grid-cols-1 gap-8">
+        {newData.map((project, index) => (
+          <ProjectCard key={project.id} project={project} index={index} />
+        ))}
+      </div>
+      <div className="flex justify-center">
+        {/* <Button onClick={() => router.push('/projects')}>
+          View More Projects
+        </Button> */}
+      </div>
+    </section>
+  );
+};
 
-// export default ProjectsSection;
+export default ProjectsSection;
 
-// const ProjectCard = ({
-//   project,
-//   index,
-// }: {
-//   project: ProjectProps;
-//   index: number;
-// }) => {
-//   const isReverse = index % 2 === 1;
+const ProjectCard = ({
+  project,
+  index,
+}: {
+  project: ProjectProps;
+  index: number;
+}) => {
+  const isReverse = index % 2 === 1;
 
-//   return (
-//     <div
-//       className={` flex flex-col xl:flex-row xl:gap-4 ${
-//         isReverse ? 'xl:flex xl:flex-row-reverse' : ''
-//       }`}
-//     >
-//       <div className="relative h-full w-full rounded-lg border border-lightTheme-text xl:max-w-[300px]">
-//         <Image
-//           src={project.imageUrl}
-//           alt={project.name}
-//           layout="fill"
-//           className="h-full w-[300px] object-cover"
-//         />
-//       </div>
-//       <div className="rounded-lg border border-dashed border-lightTheme-text p-4">
-//         <Text font="figtree" weight="bold" size="lg" className="mb-2" header>
-//           {project.name}
-//         </Text>
-//         <Text
-//           description
-//           weight="normal"
-//           font="figtree"
-//           size="md"
-//           className="mb-4 text-customGray-base"
-//         >
-//           {project.description}
-//         </Text>
-//         <div className="mb-4 flex flex-wrap gap-2">
-//           {project.tools.map((tool, index) => (
-//             <span
-//               key={index}
-//               className="cursor-pointer rounded-full bg-darkTheme-primary px-3 py-1 font-figtree text-sm text-lightTheme-text transition-all duration-700 hover:bg-transparent "
-//             >
-//               {tool}
-//             </span>
-//           ))}
-//         </div>
-//         <Link
-//           href={project.liveUrl}
-//           className="inline-flex items-center text-emerald-light hover:text-emerald-deep"
-//         >
-//           View Live Site →
-//         </Link>
-//       </div>
-//     </div>
-//   );
-// };
+  return (
+    <div
+      className={` flex flex-col xl:flex-row xl:gap-4 ${
+        isReverse ? 'xl:flex xl:flex-row-reverse' : ''
+      }`}
+    >
+      <div className="relative h-full w-full rounded-lg border border-lightTheme-text xl:max-w-[300px]">
+        <Image
+          src={project.imageUrl}
+          alt={project.name}
+          layout="fill"
+          className="h-full w-[300px] object-cover"
+        />
+      </div>
+      <div className="rounded-lg border border-dashed border-lightTheme-text p-4">
+        <Text font="figtree" weight="bold" size="lg" className="mb-2" header>
+          {project.name}
+        </Text>
+        <Text
+          description
+          weight="normal"
+          font="figtree"
+          size="md"
+          className="mb-4 text-customGray-base"
+        >
+          {project.description}
+        </Text>
+        <div className="mb-4 flex flex-wrap gap-2">
+          {project.tools.map((tool, index) => (
+            <span
+              key={index}
+              className="cursor-pointer rounded-full bg-darkTheme-primary px-3 py-1 font-figtree text-sm text-lightTheme-text transition-all duration-700 hover:bg-transparent "
+            >
+              {tool}
+            </span>
+          ))}
+        </div>
+        <Link
+          href={project.liveUrl}
+          className="inline-flex items-center text-emerald-light hover:text-emerald-deep"
+        >
+          View Live Site →
+        </Link>
+      </div>
+    </div>
+  );
+};
