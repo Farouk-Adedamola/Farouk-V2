bca761faff67bed61d95c64f55f6634a6e6e5b12
Farouk Adedamola <faroukadedamola1@gmail.com>
2025-02-13 13:44:34 +0100

    test build

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,154 +1,154 @@
-'use client';
+// 'use client';
 
-import { useRouter } from 'next/navigation';
-import React, { use, useEffect, useState } from 'react';
+// import { useRouter } from 'next/navigation';
+// import React, { use, useEffect, useState } from 'react';
 
-import axios from 'axios';
+// import axios from 'axios';
 
-import { Button } from '@/components/Button/button';
-import Socials from '@/components/SocialLinks/socials';
-import Stack from '@/components/Stack/stack';
-import Text from '@/components/Text/text';
+// import { Button } from '@/components/Button/button';
+// import Socials from '@/components/SocialLinks/socials';
+// import Stack from '@/components/Stack/stack';
+// import Text from '@/components/Text/text';
 
-export type WakatimeData = {
-  name?: string;
-  percent?: number;
-  color?: string;
-  grand_total?: {
-    human_readable_total: string;
-  };
-};
+// export type WakatimeData = {
+//   name?: string;
+//   percent?: number;
+//   color?: string;
+//   grand_total?: {
+//     human_readable_total: string;
+//   };
+// };
 
-const WAKATIME_API_URL = process.env.NEXT_PUBLIC_WAKATIME_API_URL;
-console.log(WAKATIME_API_URL);
-const WAKATIME_HOURS_API_URL = process.env.NEXT_PUBLIC_WAKATIME_HOURS_API_URL;
-console.log(WAKATIME_HOURS_API_URL);
+// const WAKATIME_API_URL = process.env.NEXT_PUBLIC_WAKATIME_API_URL;
+// console.log(WAKATIME_API_URL);
+// const WAKATIME_HOURS_API_URL = process.env.NEXT_PUBLIC_WAKATIME_HOURS_API_URL;
+// console.log(WAKATIME_HOURS_API_URL);
 
-const Hero: React.FC = () => {
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-  const [wakatimeData, setWakatimeData] = useState<WakatimeData[] | null>(null);
-  const [wakatimeHoursData, setWakatimeHoursData] = useState<
-    WakatimeData[] | null
-  >(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
+// const Hero: React.FC = () => {
+//   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+//   const [wakatimeData, setWakatimeData] = useState<WakatimeData[] | null>(null);
+//   const [wakatimeHoursData, setWakatimeHoursData] = useState<
+//     WakatimeData[] | null
+//   >(null);
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [error, setError] = useState<string | null>(null);
 
-  const router = useRouter();
+//   const router = useRouter();
 
-  const fetchWakatimeData = async () => {
-    try {
-      if (!WAKATIME_API_URL) {
-        throw new Error('Wakatime API URL is not configured');
-      }
+//   const fetchWakatimeData = async () => {
+//     try {
+//       if (!WAKATIME_API_URL) {
+//         throw new Error('Wakatime API URL is not configured');
+//       }
 
-      const response = await axios.get(WAKATIME_API_URL);
-      const data = response.data.data || response.data;
-      setWakatimeData(data);
-    } catch (err) {
-      console.error('Error fetching Wakatime data:', err);
-      setError('Failed to load Wakatime data');
-    }
-  };
+//       const response = await axios.get(WAKATIME_API_URL);
+//       const data = response.data.data || response.data;
+//       setWakatimeData(data);
+//     } catch (err) {
+//       console.error('Error fetching Wakatime data:', err);
+//       setError('Failed to load Wakatime data');
+//     }
+//   };
 
-  const fetchWakatimeHoursData = async () => {
-    try {
-      if (!WAKATIME_HOURS_API_URL) {
-        throw new Error('Wakatime Hours API URL is not configured');
-      }
+//   const fetchWakatimeHoursData = async () => {
+//     try {
+//       if (!WAKATIME_HOURS_API_URL) {
+//         throw new Error('Wakatime Hours API URL is not configured');
+//       }
 
-      const response = await axios.get(WAKATIME_HOURS_API_URL);
-      const data = response.data.data || response.data;
+//       const response = await axios.get(WAKATIME_HOURS_API_URL);
+//       const data = response.data.data || response.data;
 
-      console.log('data:', data);
-      setWakatimeHoursData(data);
-    } catch (err) {
-      console.error('Error fetching Wakatime hours data:', err);
-      setError('Failed to load Wakatime hours data');
-    } finally {
-      setIsLoading(false);
-    }
-  };
+//       console.log('data:', data);
+//       setWakatimeHoursData(data);
+//     } catch (err) {
+//       console.error('Error fetching Wakatime hours data:', err);
+//       setError('Failed to load Wakatime hours data');
+//     } finally {
+//       setIsLoading(false);
+//     }
+//   };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      await Promise.all([fetchWakatimeData(), fetchWakatimeHoursData()]);
-    };
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       await Promise.all([fetchWakatimeData(), fetchWakatimeHoursData()]);
+//     };
 
-    fetchData();
-  }, []);
+//     fetchData();
+//   }, []);
 
-  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
-    const { clientX, clientY } = e;
-    const { left, top } = e.currentTarget.getBoundingClientRect();
-    setMousePosition({
-      x: (clientX - left) / e.currentTarget.offsetWidth,
-      y: (clientY - top) / e.currentTarget.offsetHeight,
-    });
-  };
+//   const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+//     const { clientX, clientY } = e;
+//     const { left, top } = e.currentTarget.getBoundingClientRect();
+//     setMousePosition({
+//       x: (clientX - left) / e.currentTarget.offsetWidth,
+//       y: (clientY - top) / e.currentTarget.offsetHeight,
+//     });
+//   };
 
-  return (
-    <section
-      className=" flex w-full flex-col sm:pb-10 max-md:gap-6 md:flex-row xl:items-center xl:justify-between"
-      onMouseMove={handleMouseMove}
-    >
-      <div className="flex flex-col justify-start gap-2 xl:max-w-[600px] xl:items-start">
-        <div className="flex flex-col items-center justify-start xl:items-start">
-          <Text
-            weight="bold"
-            size="xs"
-            font="figtree"
-            className=" m-0 font-figtree font-bold"
-          >
-            Farouk Adedamola
-          </Text>
-          <div className=' relative  inline-block w-fit  rounded-md bg-[#090D26] px-1.5 py-0.5  before:pointer-events-none before:absolute before:left-0 before:top-0 before:z-10 before:h-full before:w-full before:bg-[url("/noise.gif")] before:opacity-[0.09] before:content-[""]'>
-            <Text
-              weight="bold"
-              size="xl"
-              font="figtree"
-              className=" bg-gradient-to-t     from-[#a2b6fa] to-[#334cec] bg-clip-text text-transparent  "
-            >
-              Frontend Engineer
-            </Text>
-          </div>
-        </div>
-        <div>
-          <Text
-            weight="bold"
-            font="inter"
-            size="md"
-            className="text-center xl:text-left"
-          >
-            I specialize in building elegant, accessible interfaces where
-            thoughtful design meets clean code for the web.
-          </Text>
-        </div>
-        <div className="max-md:flex max-md:justify-center">
-          <Button onClick={() => router.push('/about')}>More about me</Button>
-        </div>
-        <div className="mt-4 flex items-center justify-center xl:justify-start">
-          <Socials socialStyles="gap-6" />
-        </div>
-      </div>
-      <div>
-        {error ? (
-          <Text className="text-error">{error}</Text>
-        ) : isLoading ? (
-          <div className="flex items-center justify-start">
-            <Text className="text-left">Loading...</Text>
-          </div>
-        ) : (
-          wakatimeData &&
-          wakatimeData.length > 0 && (
-            <Stack
-              code_hours={wakatimeHoursData}
-              data={wakatimeData.slice(0, 6)}
-            />
-          )
-        )}
-      </div>
-    </section>
-  );
-};
+//   return (
+//     <section
+//       className=" flex w-full flex-col sm:pb-10 max-md:gap-6 md:flex-row xl:items-center xl:justify-between"
+//       onMouseMove={handleMouseMove}
+//     >
+//       <div className="flex flex-col justify-start gap-2 xl:max-w-[600px] xl:items-start">
+//         <div className="flex flex-col items-center justify-start xl:items-start">
+//           <Text
+//             weight="bold"
+//             size="xs"
+//             font="figtree"
+//             className=" m-0 font-figtree font-bold"
+//           >
+//             Farouk Adedamola
+//           </Text>
+//           <div className=' relative  inline-block w-fit  rounded-md bg-[#090D26] px-1.5 py-0.5  before:pointer-events-none before:absolute before:left-0 before:top-0 before:z-10 before:h-full before:w-full before:bg-[url("/noise.gif")] before:opacity-[0.09] before:content-[""]'>
+//             <Text
+//               weight="bold"
+//               size="xl"
+//               font="figtree"
+//               className=" bg-gradient-to-t     from-[#a2b6fa] to-[#334cec] bg-clip-text text-transparent  "
+//             >
+//               Frontend Engineer
+//             </Text>
+//           </div>
+//         </div>
+//         <div>
+//           <Text
+//             weight="bold"
+//             font="inter"
+//             size="md"
+//             className="text-center xl:text-left"
+//           >
+//             I specialize in building elegant, accessible interfaces where
+//             thoughtful design meets clean code for the web.
+//           </Text>
+//         </div>
+//         <div className="max-md:flex max-md:justify-center">
+//           <Button onClick={() => router.push('/about')}>More about me</Button>
+//         </div>
+//         <div className="mt-4 flex items-center justify-center xl:justify-start">
+//           <Socials socialStyles="gap-6" />
+//         </div>
+//       </div>
+//       <div>
+//         {error ? (
+//           <Text className="text-error">{error}</Text>
+//         ) : isLoading ? (
+//           <div className="flex items-center justify-start">
+//             <Text className="text-left">Loading...</Text>
+//           </div>
+//         ) : (
+//           wakatimeData &&
+//           wakatimeData.length > 0 && (
+//             <Stack
+//               code_hours={wakatimeHoursData}
+//               data={wakatimeData.slice(0, 6)}
+//             />
+//           )
+//         )}
+//       </div>
+//     </section>
+//   );
+// };
 
-export default Hero;
+// export default Hero;
--- src/components/posts/posts-grid.tsx
+++ src/components/posts/posts-grid.tsx
@@ -1,72 +1,72 @@
-'use client';
+// 'use client';
 
-import { useRouter } from 'next/navigation';
-import { useRef } from 'react';
+// import { useRouter } from 'next/navigation';
+// import { useRef } from 'react';
 
-import Text from '../Text/text';
-import { Button } from '@/components/Button/button';
-import Paginate from '@/components/paginate';
-import PostCard from '@/components/posts/post-card';
-import usePosts from '@/hooks/use-posts';
-import { Post } from '@/types/post';
+// import Text from '../Text/text';
+// import { Button } from '@/components/Button/button';
+// import Paginate from '@/components/paginate';
+// import PostCard from '@/components/posts/post-card';
+// import usePosts from '@/hooks/use-posts';
+// import { Post } from '@/types/post';
 
-export default function PostsGrid({
-  allPosts,
-  paginate,
-  moreBtn,
-}: {
-  allPosts: Post[];
-  paginate?: boolean;
-  moreBtn?: boolean;
-}) {
-  const { posts, totalPages } = usePosts(allPosts);
-  const rootRef = useRef<HTMLDivElement>(null);
+// export default function PostsGrid({
+//   allPosts,
+//   paginate,
+//   moreBtn,
+// }: {
+//   allPosts: Post[];
+//   paginate?: boolean;
+//   moreBtn?: boolean;
+// }) {
+//   const { posts, totalPages } = usePosts(allPosts);
+//   const rootRef = useRef<HTMLDivElement>(null);
 
-  const router = useRouter();
+//   const router = useRouter();
 
-  return (
-    <section
-      ref={rootRef}
-      className={`flex w-full scroll-mt-12 flex-col items-center ${
-        moreBtn && 'section_layout'
-      } `}
-    >
-      {moreBtn && (
-        <div className="self-start">
-          <Text
-            font="figtree"
-            weight="bold"
-            size="xs"
-            className="mb-8 font-bold "
-          >
-            Selected Posts{' '}
-            <span className="inline-block h-4 w-4 rounded-full bg-emerald-light"></span>
-          </Text>
-        </div>
-      )}
+//   return (
+//     <section
+//       ref={rootRef}
+//       className={`flex w-full scroll-mt-12 flex-col items-center ${
+//         moreBtn && 'section_layout'
+//       } `}
+//     >
+//       {moreBtn && (
+//         <div className="self-start">
+//           <Text
+//             font="figtree"
+//             weight="bold"
+//             size="xs"
+//             className="mb-8 font-bold "
+//           >
+//             Selected Posts{' '}
+//             <span className="inline-block h-4 w-4 rounded-full bg-emerald-light"></span>
+//           </Text>
+//         </div>
+//       )}
 
-      {posts.length ? (
-        <ul
-          id="posts-grid"
-          className={`mb-[64px] grid w-full grid-cols-1 items-start justify-start gap-x-8  gap-y-32 md:grid-cols-2 xl:grid-cols-3 ${
-            moreBtn ? '!gap-y-8' : 'gap-y-32'
-          }`}
-        >
-          {posts.map((post) => (
-            <li key={post.slug}>
-              <PostCard post={post} />
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p className="mt-10 text-center text-lg">No matching posts found</p>
-      )}
-      {moreBtn && (
-        <Button onClick={() => router.push('/blog')}>More Posts</Button>
-      )}
-      {paginate && (
-        <Paginate totalPages={totalPages} elementToScroll={rootRef.current} />
-      )}
-    </section>
-  );
-}
+//       {posts.length ? (
+//         <ul
+//           id="posts-grid"
+//           className={`mb-[64px] grid w-full grid-cols-1 items-start justify-start gap-x-8  gap-y-32 md:grid-cols-2 xl:grid-cols-3 ${
+//             moreBtn ? '!gap-y-8' : 'gap-y-32'
+//           }`}
+//         >
+//           {posts.map((post) => (
+//             <li key={post.slug}>
+//               <PostCard post={post} />
+//             </li>
+//           ))}
+//         </ul>
+//       ) : (
+//         <p className="mt-10 text-center text-lg">No matching posts found</p>
+//       )}
+//       {moreBtn && (
+//         <Button onClick={() => router.push('/blog')}>More Posts</Button>
+//       )}
+//       {paginate && (
+//         <Paginate totalPages={totalPages} elementToScroll={rootRef.current} />
+//       )}
+//     </section>
+//   );
+// }
